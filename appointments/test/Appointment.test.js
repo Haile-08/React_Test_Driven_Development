2e02268719ade309f71a473e6366ec9e9cac6b5e
Haile <haile4cmd@gmail.com>
2024-06-13 14:45:27 +0300

    build: add list value to the component

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -2,6 +2,7 @@ import React, { act } from "react";
 import ReactDOM from "react-dom/client"
 import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
+
 describe("Appointment",()=>{
     let container;
 
@@ -68,5 +69,22 @@ describe("AppointmentsDayView", () => {
         render(<AppointmentsDayView appointments={twoAppointments}/>);
         const listChildren = document.querySelectorAll("ol > li");
         expect(listChildren).toHaveLength(2);
+    });
+
+    it("renders the time of each appointment", () => {
+        const today = new Date();
+        const twoAppointments = [
+            { startsAt: today.setHours(12, 0)},
+            { startsAt: today.setHours(13, 0)},
+        ];
+
+        render(<AppointmentsDayView appointments={twoAppointments}/>);
+        const listChildren = document.querySelectorAll("li");
+        expect(listChildren[0].textContent).toEqual(
+            "12:00"
+        );
+        expect(listChildren[1].textContent).toEqual(
+            "13:00"
+        );
     });
 });
