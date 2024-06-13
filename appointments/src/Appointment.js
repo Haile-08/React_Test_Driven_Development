b57e82c355904970c6556649e7e7474629f51227
Haile <haile4cmd@gmail.com>
2024-06-13 16:10:14 +0300

    build: impelement usestate

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const appointmentTimeOfDay = (startsAt) => {
     const [h, m] = new Date(startsAt).toTimeString().split(":");
@@ -9,21 +9,27 @@ export const Appointment = ({ customer }) => (
     <div>{customer.firstName}</div>
 );
 
-export const AppointmentsDayView = ({ appointments }) => (
+export const AppointmentsDayView = ({ appointments }) => {
+   const [selectedAppointment, setSelectedAppointment] = useState(0);
+
+   return ( 
     <div id="appointmentsDayView">
         {appointments.length === 0 ? (
             <p>There are no appointments scheduled for today.</p>
         ): (
-            <Appointment {...appointments[0]} />
+            <Appointment {...appointments[selectedAppointment]} />
         )}
         <ol>
-            {appointments.map(appointment => (
+            {appointments.map((appointment, i) => (
                 <li key={appointment.startsAt}>
-                    <button type="button">
+                    <button 
+                        type="button"
+                        onClick={() => setSelectedAppointment(i)}
+                    >
                         {appointmentTimeOfDay(appointment.startsAt)}
                     </button>
                 </li>
             ))}
         </ol>
     </div>
-);
+)};
